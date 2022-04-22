9636888985e0b7e35522a80751b1cfbc9dbd7d20
emaresmoreno <emaresmoreno@ucsd.edu>
2022-04-22 13:25:04 -0700

    created a tester for MarkdownParse

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,5 +1,9 @@
 import static org.junit.Assert.*;//imports junit.Assert which is needed to use assertEquals
 import org.junit.*;//it imports jUnit
+import java.nio.file.Files;
+import java.util.*;
+import java.nio.file.Path;
+import java.io.IOException;
 
 public class MarkdownParseTest {//This is the class where are the testing methods will be held
     @Test//lets jUnit know this is a test
@@ -7,5 +11,13 @@ public class MarkdownParseTest {//This is the class where are the testing method
         assertEquals(2, 1 + 1);//This checks that the (expected answer, what we try to check) matches
     }
 
-    
+    @Test
+    public void checkLinks() throws IOException {
+            Path result = Path.of("test-file.md");//gets a path to test-file.md (says it exixts) (Needed for line 17)
+            String contents = Files.readString(result);
+            //contents gets the information that is stored in result
+            ArrayList<String> inputResult = MarkdownParse.getLinks(contents);//puts wtv is stored in result (now called contents) into the class we created
+            assertEquals(inputResult, List.of("https://something.com", "some-thing.html"));//checks expected and actual output 
+    }
 }
+//The error on line 17 cannot be removed. I tried to test the checkLinks test using the red circle and the error appeared. 
