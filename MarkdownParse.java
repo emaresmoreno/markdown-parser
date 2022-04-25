e953d6cdf52e0ba63c6e2a65f83e3ffcd72996dc
emaresmoreno <emaresmoreno@ucsd.edu>
2022-04-24 21:10:44 -0700

    fixed image error
    Return:[https://something.com, some-thing.html]
    It does not read the image as a link

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -16,9 +16,12 @@ public class MarkdownParse {
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", currentIndex);
             int closeParen = markdown.indexOf(")", openParen);
-            if ((openBracket == -1) || (closeBracket == -1) || (openParen == -1) || (closeParen == -1)){
-                currentIndex += 1;
+            if (markdown.charAt(openBracket - 1) == '!'){
+                currentIndex = closeParen + 1;
             }
+            /*if ((openBracket == -1) || (closeBracket == -1) || (openParen == -1) || (closeParen == -1)){
+                currentIndex += 1;
+            }*/
             else{
                 toReturn.add(markdown.substring(openParen + 1, closeParen));
                 currentIndex = closeParen + 1;
